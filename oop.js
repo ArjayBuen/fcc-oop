4df3ffdac970a591fd1b53ff97fe0da410429745
ArjayBuen <arjaybuen10@gmail.com>
2023-11-04 23:46:24 +0800

    continuation learning oop

--- oop.js
+++ oop.js
@@ -25,3 +25,38 @@ function Dog(name, color) {
   }
   
   let terrier= new Dog("Blacky", "black");
+
+
+  function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+  }
+  
+  // Only change code below this line
+  let myHouse= new House(4);
+  
+  myHouse instanceof House;
+
+  
+  function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  // Only change code below this line
+  for (let property in canary) {
+    if(canary.hasOwnProperty(property)) {
+      ownProps.push(property);
+    }
+  }
+
+
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs=4;
+  
+  // Only change code above this line
+  let beagle = new Dog("Snoopy");
